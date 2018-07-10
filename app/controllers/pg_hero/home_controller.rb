b7da8f121dc486014482bc51532bdeca56b5f567
Naisila Puka <naisila@citusdata.com>
2018-07-10 11:01:15 +0300

    citus compatible tune tab

--- app/controllers/pg_hero/home_controller.rb
+++ app/controllers/pg_hero/home_controller.rb
@@ -264,8 +264,12 @@ module PgHero
     def tune
       @title = "Tune"
       @settings = @database.settings
-      @autovacuum_settings = @database.autovacuum_settings if params[:autovacuum]
       @citus_enabled = @database.citus_enabled?
+      if @citus_enabled
+        @worker_settings = @database.worker_settings
+      end
+      @autovacuum_settings = @database.autovacuum_settings if params[:autovacuum]
+      
     end
 
     def connections
--- lib/pghero/methods/settings.rb
+++ lib/pghero/methods/settings.rb
@@ -1,6 +1,7 @@
 module PgHero
   module Methods
     module Settings
+
       def settings
         names =
           if server_version_num >= 90500
@@ -19,6 +20,24 @@ module PgHero
         fetch_settings(names)
       end
 
+      def citus_worker_settings
+        citus_worker_names =
+          if server_version_num >= 90500
+            %i(
+              max_connections shared_buffers effective_cache_size work_mem
+              maintenance_work_mem min_wal_size max_wal_size checkpoint_completion_target
+              wal_buffers default_statistics_target
+            )
+          else
+            %i(
+              max_connections shared_buffers effective_cache_size work_mem
+              maintenance_work_mem checkpoint_segments checkpoint_completion_target
+              wal_buffers default_statistics_target
+            )
+          end
+        citus_fetch_worker_settings(citus_worker_names)
+      end
+         
       def autovacuum_settings
         fetch_settings %i(autovacuum autovacuum_max_workers autovacuum_vacuum_cost_limit autovacuum_vacuum_scale_factor autovacuum_analyze_scale_factor)
       end
@@ -31,6 +50,10 @@ module PgHero
 
       def fetch_settings(names)
         Hash[names.map { |name| [name, select_one("SHOW #{name}")] }]
+      end
+
+      def citus_fetch_worker_settings(citus_worker_names)
+        Hash[citus_worker_names.map { |citus_worker_name| [citus_worker_name, select_one("SELECT result FROM run_command_on_workers($cmd$ SHOW #{citus_worker_name} $cmd$) LIMIT 1")] }]
       end
     end
   end
