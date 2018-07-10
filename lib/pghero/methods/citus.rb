0a3409674ada36e548ea4adfe24a7fa27f0c0d9c
Naisila Puka <naisila@citusdata.com>
2018-07-10 11:01:15 +0300

    Add settings for a worker in the Tune tab if citus is enabled

--- lib/pghero/methods/citus.rb
+++ lib/pghero/methods/citus.rb
@@ -9,6 +9,10 @@ module PgHero
       def citus_readable?
         select_one("SELECT EXISTS(SELECT * FROM pg_extension WHERE extname = 'citus')")
       end
+      
+      def citus_worker_count
+        @citus_worker_count = select_one("SELECT COUNT(*) FROM master_get_active_worker_nodes()")
+      end
     end
   end
 end
